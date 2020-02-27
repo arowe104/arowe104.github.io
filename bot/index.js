d3619edf5c84f862f2aa54e8b268717c00b3a568
Alexander (“Alex”) Rowe <student@macadmicbook235.dalton.org>
2020-02-27 13:05:45 -0500

    added bot

---
+++ bot/index.js
@@ -0,0 +1,17 @@
+var Twit = require('twit')
+var T = new Twit({
+    consumer_key:         'f4gH7moFR1brwAgoTRZ5jupUF',
+    consumer_secret:      'bMw1Gae3FGVVjOyxk1TX4Y2R1gJ1ckdlaUVDGc70zpdL0e71y5',
+    access_token:         '1228370535727366147-aq6lGMbKeLm9KyPZAPNuE5NTbFUyjy',
+    access_token_secret:  'jccQbVQXevppP1ovdOZxVzxwFDab46Nf2iR7Bv9RtGN0G',
+})
+var users = ["1228370535727366147"];
+var stream = T.stream('statuses/filter', {follow: users});
+stream.on('tweet', function (tweet) {
+    if (users.indexOf(tweet.user.id_str) > -1) {
+        console.log(tweet.user.name + ": " + tweet.text);
+        T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
+            console.log(data)
+        })
+    }
+})
